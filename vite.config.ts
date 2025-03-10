29a1828883085b4b3094efb47d996d07231ef9fb
Aleksandr Petrov <petrov.aleksr@gmail.com>
2025-03-10 21:59:57 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 'template-react-project' для gh-pages
+  base: '/', // '/adaptive-image-react' для gh-pages
 });
